fd1c1c703de4c5b663476191c7ba54ebc862c6e1
Matthias Langwieser <37292244+lamacoding@users.noreply.github.com>
2024-02-15 16:03:48 +0100

    Add mySQL connection

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -1,39 +1,67 @@
 import bodyParser from "body-parser";
 import express, { response } from "express";
 import path from "path";
-import https from "https";
-import fs from "fs";
-import mime from "mime";
+// import https from "https";
+// import fs from "fs";
+// import mime from "mime";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
+import mysql from "mysql";
 
 const app = express();
-const options = {
-  key: fs.readFileSync(
-    "/etc/letsencrypt/live/y3hqfr.myvserver.online/privkey.pem"
-  ),
-  cert: fs.readFileSync(
-    "/etc/letsencrypt/live/y3hqfr.myvserver.online/fullchain.pem"
-  ),
-};
+const dbConnection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "todo",
+});
+const query = "SELECT * FROM task WHERE userID = 1";
 
-const PORT = 443;
-const __dirname = dirname(fileURLToPath(import.meta.url));
-
-app.use(express.static(path.join(__dirname, 'public'), {
-  setHeaders: (res, filePath) => {
-      res.setHeader('Content-Type', mime.getType(filePath) || 'text/plain');
-  },
-}));
-app.use(bodyParser.json());
-app.use(express.static("../frontend"));
-
-app.get("/", (req, res) => {
-  res.status(200).sendFile(path.join(__dirname, "../frontend/login.html"));
+dbConnection.connect((err) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log("Connected to the database");
+  }
 });
 
-const server = https.createServer(options, app);
+// const options = {
+//   key: fs.readFileSync(
+//     "/etc/letsencrypt/live/y3hqfr.myvserver.online/privkey.pem"
+//   ),
+//   cert: fs.readFileSync(
+//     "/etc/letsencrypt/live/y3hqfr.myvserver.online/fullchain.pem"
+//   ),
+// };
 
-server.listen(PORT, () => {
-  console.log(`Server running on https://y3hqfr.myvserver.online:${PORT}`);
+const PORT = 3000;
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+//app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.json());
+app.use(express.static("../FrontEnd"));
+
+app.get("/", (req, res) => {
+  res.status(200).sendFile(path.join(__dirname, "../FrontEnd/login.html"));
+});
+
+app.get("/todo", (req, res) => {
+  res.status(200).sendFile(path.join(__dirname, "../FrontEnd/todo.html"));
+  dbConnection.query(query, (err, result) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log(result);
+    }
+  });
+});
+
+process.on("beforeExit", () => {
+  dbConnection.end();
+  console.log("Database connection closed");
+});
+// const server = https.createServer(options, app);
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
 });
